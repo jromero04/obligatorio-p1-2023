1cce0645f785f8b50d87a4a61a29db401b919b8d
jromero04 <151089020+jromero04@users.noreply.github.com>
2023-11-16 22:19:13 -0300

    Add files via upload

---
+++ Consultas/consulta_nro_5.py
@@ -0,0 +1,20 @@
+from Clases.claseJefe import Jefe
+from Clases.claseEquipo import Equipo
+from amain import listaTotalEquipos
+
+#def consulta_5(listaTotalEquipos):
+    #lista_desordenada = []
+    #for aux in listaTotalEquipos:
+    #    ci_jefe = aux.
+
+    
+    
+    
+    #lista_desordenada = []
+    #aux = Equipo(None, None, [None,None,None,None,None,None,None,None,None,None,None, ci_jefe])
+    #for aux in listaTotalEquipos:
+    #    lista_desordenada.append(ci_jefe)
+    
+    #lista_filtrada = [obj for obj in lista_para_buscar if isinstance(obj, Jefe)]
+    #for i in lista_filtrada:
+    #    print(i)
