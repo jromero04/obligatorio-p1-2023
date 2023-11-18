a4cb251d0b1baea9f6e8c7ee409dd96e15b5717f
Mfernandez1934 <149985109+Mfernandez1934@users.noreply.github.com>
2023-11-17 23:57:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,9 @@ listaTotalEmpleados = []
 listaTotalAutos = []
 listaTotalEquipos = []
 lista_empleados_registrados = []
+lista_habilitados = []
+score_final = 0
 menu_activado = True
 
-menuEjecucion(menu_activado, listaTotalEmpleados, listaTotalAutos, listaTotalEquipos, lista_empleados_registrados)
-
-    #### en el main van todos los inputs y en el gestor (hay que crear) van todas las funciones que
-    #### no requieren validaciones y que son creadas con los datos que salen del main
-    #### clase carrera tiene metodo simular_carrera
-    #### crear <<ejemplos.txt>>
-    #error consulta4
-    #archivo texto
-    #while de altas
-    
-    #main importa al menu y el menu importa a todas las funciones
+menuEjecucion(menu_activado, listaTotalEmpleados, listaTotalAutos, listaTotalEquipos, lista_empleados_registrados, lista_habilitados, score_final)
     
