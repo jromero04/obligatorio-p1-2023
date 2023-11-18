b2c1f68020ea7cc582c9269bdf687111575c712e
Mfernandez1934 <149985109+Mfernandez1934@users.noreply.github.com>
2023-11-18 00:00:54 -0300

    Add files via upload

---
+++ archivos_prueba.py
@@ -0,0 +1,52 @@
+from FIA import Fia
+from Menu.menu_principal import menu_principal
+from Menu.menu_consultas import menu_consultas
+
+
+    #Lista de pruebitas para las funciones, están diseñadas para funcionar con el archivo "zentrada.txt"
+
+
+#def menuEjecucion(menu_activado, listaTotalEmpleados, listaTotalAutos, listaTotalEquipos, lista_empleados_registrados, lista_habilitados, score_final):
+    
+    #f = Fia()
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_empleado(listaTotalEmpleados)
+    #f.alta_auto(listaTotalAutos)
+    #f.alta_auto(listaTotalAutos)
+    #f.alta_auto(listaTotalAutos)
+    #f.alta_equipo(listaTotalEquipos, listaTotalAutos, listaTotalEmpleados)
+    #f.empleados_registrados(listaTotalEmpleados, lista_empleados_registrados, listaTotalEquipos)
+    #f.consulta_4(lista_empleados_registrados)
+    #f.consulta_5(lista_empleados_registrados)
+    #f.consulta_3(lista_empleados_registrados)
+    #f.pilotos_lesionados(lista_empleados_registrados, lista_habilitados, score_final)
+    #f.alta_equipo(listaTotalEquipos, listaTotalAutos, listaTotalEmpleados)
+    #f.empleados_registrados(listaTotalEmpleados, lista_empleados_registrados, listaTotalEquipos)
+    #f.pilotos_lesionados(lista_empleados_registrados, lista_habilitados, score_final)
+    #f.pilotos_abandonan(lista_habilitados, score_final)
+    #f.calcular_suma_score_mecanicos(lista_empleados_registrados, listaTotalEquipos)
+    #f.calcular_suma_score_pilotos(lista_empleados_registrados, listaTotalEquipos)
+    #f.obtener_scores_autos_equipos(listaTotalEquipos, listaTotalAutos)
+    #f.simular_carrera(lista_empleados_registrados, listaTotalEquipos, listaTotalAutos)
